050da8bfbab47e0e69cf9b8f8ba183910cabc143
Yuri Vishnevsky <yurivish@gmail.com>
2026-04-22 03:34:26 +0000

    datapotamus: daemon

---
+++ src/toolkit/datapotamus/daemon.clj
@@ -0,0 +1,104 @@
+(ns toolkit.datapotamus.daemon
+  (:require [clojure.core.async :as a]
+            [com.stuartsierra.component :as component]
+            [toolkit.datapotamus.registry :as reg]
+            [toolkit.datapotamus.runner :as runner]
+            [toolkit.datapotamus.store :as store]
+            [toolkit.filewatcher :as fw])
+  (:import [java.nio.file Files LinkOption Path Paths]
+           [java.nio.file.attribute BasicFileAttributes]))
+
+(set! *warn-on-reflection* true)
+
+(defn- ^Path ->path [^String s] (Paths/get s (into-array String [])))
+
+(defn- stat [^Path p]
+  (when-let [^BasicFileAttributes attrs
+             (try (Files/readAttributes p BasicFileAttributes
+                                        ^"[Ljava.nio.file.LinkOption;" (into-array LinkOption []))
+                  (catch Throwable _ nil))]
+    {:mtime (.toMillis (.lastModifiedTime attrs))
+     :size  (.size attrs)}))
+
+(defn- spawn-run! [{:keys [datasource events-ch pipeline idle-complete-ms registry]}
+                   slug ^String abs-path]
+  (let [p   (->path abs-path)
+        s   (stat p)
+        rid (random-uuid)
+        prior (reg/install! registry slug {:run-id rid})]
+    (when prior
+      (store/update-run! datasource (:run-id prior)
+                         {:state :cancelled
+                          :finished-at (System/currentTimeMillis)}))
+    (store/insert-run! datasource
+      {:run-id rid :pipeline-id (:pipeline-id pipeline)
+       :input-path abs-path :input-slug slug
+       :input-mtime (:mtime s) :input-size (:size s)
+       :state :pending :started-at (System/currentTimeMillis)})
+    (a/thread
+      (try
+        (runner/run-pipeline!
+          {:datasource datasource :events-ch events-ch :run-id rid
+           :pipeline pipeline
+           :seed {:data {:path abs-path :slug slug}}
+           :idle-complete-ms idle-complete-ms})
+        (finally (reg/remove-if-matches! registry slug rid))))))
+
+(defn- handle! [{:keys [watch-dir registry] :as ctx} {:keys [path dir?]}]
+  (if dir?
+    ;; Directory change: find files whose slugs have no live registry entry.
+    (let [root (->path path)
+          ^"[Ljava.nio.file.FileVisitOption;" no-opts (into-array java.nio.file.FileVisitOption [])]
+      (try
+        (doseq [^Path child (iterator-seq (.iterator (Files/walk root no-opts)))
+                :when (not (Files/isDirectory child
+                             ^"[Ljava.nio.file.LinkOption;" (into-array LinkOption [])))]
+          (let [abs  (str child)
+                slug (str (.relativize (->path watch-dir) child))]
+            (when-not (reg/current registry slug)
+              (spawn-run! ctx slug abs))))
+        (catch Throwable _ nil)))
+    ;; File content change.
+    (let [slug (str (.relativize (->path watch-dir) (->path path)))]
+      (spawn-run! ctx slug path))))
+
+(defrecord PipelineDaemon [watch-dir datasource events-ch pipeline
+                           stable-gap-ms idle-complete-ms
+                           fw registry stop-ch done-ch]
+  component/Lifecycle
+  (start [this]
+    (let [fw' (-> (fw/make {:interval-ms 50 :safety-gap-ms stable-gap-ms
+                             :changes-buffer 64})
+                  (fw/watch-dir-recursive watch-dir)
+                  fw/start)
+          r    (reg/make)
+          stop (a/chan)
+          done (a/chan)
+          ctx  {:watch-dir watch-dir :datasource datasource
+                :events-ch events-ch :pipeline pipeline
+                :idle-complete-ms idle-complete-ms :registry r}]
+      (a/thread
+        (try
+          (loop []
+            (let [ch    (fw/changes fw')
+                  [v c] (a/alts!! [ch stop])]
+              (when (and (= c ch) (some? v))
+                (try (handle! ctx v)
+                     (catch Throwable ex
+                       (binding [*out* *err*]
+                         (println "datapotamus daemon error:" (ex-message ex)))))
+                (recur))))
+          (finally (a/close! done))))
+      (assoc this :fw fw' :registry r :stop-ch stop :done-ch done)))
+  (stop [this]
+    (when stop-ch (a/close! stop-ch))
+    (when done-ch (a/<!! done-ch))
+    (when fw (fw/stop fw))
+    (assoc this :fw nil :registry nil :stop-ch nil :done-ch nil)))
+
+(defn make
+  [{:keys [stable-gap-ms idle-complete-ms]
+    :or {stable-gap-ms 3000 idle-complete-ms 500} :as opts}]
+  (map->PipelineDaemon (assoc opts
+                              :stable-gap-ms stable-gap-ms
+                              :idle-complete-ms idle-complete-ms)))
---
+++ src/toolkit/datapotamus/daemon_test.clj
@@ -0,0 +1,69 @@
+(ns toolkit.datapotamus.daemon-test
+  (:require [clojure.string :as str]
+            [clojure.test :refer [deftest is use-fixtures]]
+            [com.stuartsierra.component :as component]
+            [next.jdbc :as jdbc]
+            [toolkit.datapotamus.daemon :as daemon]
+            [toolkit.datapotamus.proc :as proc]
+            [toolkit.datapotamus.sinks :as sinks]
+            [toolkit.datapotamus.store :as store]
+            [toolkit.datapotamus.trace :as trace])
+  (:import [java.io File]
+           [java.nio.file Files Path]
+           [java.nio.file.attribute FileAttribute FileTime]))
+
+(def ^:dynamic *ds* nil)
+(def ^:dynamic ^Path *dir* nil)
+
+(defn- tmp-db ^File []
+  (-> (Files/createTempFile "dp-daemon-" ".sqlite" (into-array FileAttribute []))
+      .toFile))
+
+(defn fix [t]
+  (let [f (tmp-db)
+        d (Files/createTempDirectory "dp-daemon-" (into-array FileAttribute []))
+        ds (jdbc/get-datasource {:dbtype "sqlite" :dbname (.getAbsolutePath f)})]
+    (try
+      (store/migrate! ds)
+      (jdbc/execute! ds ["CREATE TABLE results (
+                           rowid INTEGER PRIMARY KEY AUTOINCREMENT,
+                           run_id TEXT, msg_id TEXT, data_id TEXT,
+                           written_at INTEGER, data TEXT)"])
+      (binding [*ds* ds *dir* d] (t))
+      (finally
+        (.delete f)
+        (doseq [p (reverse (iterator-seq
+                              (.iterator (Files/walk d (into-array java.nio.file.FileVisitOption [])))))]
+          (try (Files/delete p) (catch Throwable _)))))))
+
+(use-fixtures :each fix)
+
+(defn- write-aged! [^Path p ^String s]
+  (Files/write p (.getBytes s "UTF-8") (into-array java.nio.file.OpenOption []))
+  (Files/setLastModifiedTime
+    p (FileTime/fromMillis (- (System/currentTimeMillis) 10000))))
+
+(defn- read-int [m]
+  (assoc m :data (Integer/parseInt (str/trim (slurp (:path (:data m)))))))
+
+(deftest daemon-processes-file-drop-end-to-end
+  (let [tc (component/start (trace/make {:datasource *ds* :batch-size 10 :flush-ms 20}))
+        pipeline {:pipeline-id :demo :entry :read
+                  :procs {:read (proc/step-proc :read read-int)
+                          :dec  (proc/step-proc :dec (fn [m] (update m :data dec)))
+                          :sink (sinks/sqlite-sink :sink
+                                 {:table :results :datasource *ds*})}
+                  :conns [[[:read :out] [:dec :in]]
+                          [[:dec :out]  [:sink :in]]]}
+        dae (component/start
+              (daemon/make {:watch-dir (str *dir*) :datasource *ds*
+                            :events-ch (:events-ch tc) :pipeline pipeline
+                            :stable-gap-ms 50 :idle-complete-ms 200}))]
+    (try
+      (write-aged! (.resolve *dir* "n.txt") "10\n")
+      (Thread/sleep 1500)
+      (let [runs (store/list-runs *ds* 10)]
+        (is (= 1 (count runs)))
+        (is (= :completed (:state (first runs))))
+        (is (= 1 (count (jdbc/execute! *ds* ["SELECT * FROM results"])))))
+      (finally (component/stop dae) (component/stop tc)))))
