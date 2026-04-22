639a9d3231134190470d52dda5ceafc6e5b393a7
Yuri Vishnevsky <yurivish@gmail.com>
2026-04-22 03:14:07 +0000

    datapotamus: trace collector

---
+++ src/toolkit/datapotamus/trace.clj
@@ -0,0 +1,43 @@
+(ns toolkit.datapotamus.trace
+  (:require [clojure.core.async :as a]
+            [com.stuartsierra.component :as component]
+            [toolkit.datapotamus.store :as store]))
+
+(set! *warn-on-reflection* true)
+
+(defrecord TraceCollector [datasource batch-size flush-ms events-ch stop-ch done-ch]
+  component/Lifecycle
+  (start [this]
+    (let [events-ch (or events-ch (a/chan 256))
+          stop-ch   (a/chan)
+          done-ch   (a/chan)]
+      (a/thread
+        (try
+          (loop [batch [] timer (a/timeout flush-ms)]
+            (let [[v ch] (a/alts!! [events-ch timer stop-ch])]
+              (cond
+                (or (= ch stop-ch) (nil? v))
+                (store/insert-events! datasource batch)
+
+                (= ch timer)
+                (do (store/insert-events! datasource batch)
+                    (recur [] (a/timeout flush-ms)))
+
+                :else
+                (let [batch' (conj batch v)]
+                  (if (>= (count batch') batch-size)
+                    (do (store/insert-events! datasource batch')
+                        (recur [] (a/timeout flush-ms)))
+                    (recur batch' timer))))))
+          (finally (a/close! done-ch))))
+      (assoc this :events-ch events-ch :stop-ch stop-ch :done-ch done-ch)))
+  (stop [this]
+    (when stop-ch (a/close! stop-ch))
+    (when done-ch (a/<!! done-ch))
+    (assoc this :events-ch nil :stop-ch nil :done-ch nil)))
+
+(defn make
+  [{:keys [datasource batch-size flush-ms]
+    :or {batch-size 100 flush-ms 50}}]
+  (map->TraceCollector {:datasource datasource
+                         :batch-size batch-size :flush-ms flush-ms}))
---
+++ src/toolkit/datapotamus/trace_test.clj
@@ -0,0 +1,40 @@
+(ns toolkit.datapotamus.trace-test
+  (:require [clojure.core.async :as a]
+            [clojure.test :refer [deftest is use-fixtures]]
+            [com.stuartsierra.component :as component]
+            [next.jdbc :as jdbc]
+            [toolkit.datapotamus.store :as store]
+            [toolkit.datapotamus.trace :as trace])
+  (:import [java.io File]
+           [java.nio.file Files]
+           [java.nio.file.attribute FileAttribute]))
+
+(def ^:dynamic *ds* nil)
+
+(defn- tmp-db ^File []
+  (-> (Files/createTempFile "dp-trace-" ".sqlite" (into-array FileAttribute []))
+      .toFile))
+
+(defn with-db [t]
+  (let [f (tmp-db)
+        ds (jdbc/get-datasource {:dbtype "sqlite" :dbname (.getAbsolutePath f)})]
+    (try (store/migrate! ds) (binding [*ds* ds] (t))
+         (finally (.delete f)))))
+
+(use-fixtures :each with-db)
+
+(defn- seed [ds run-id]
+  (store/insert-run! ds {:run-id run-id :pipeline-id :d
+                          :input-path "p" :input-slug "p"
+                          :state :running :started-at 0}))
+
+(deftest collector-batches-events-to-sqlite
+  (let [run-id (random-uuid) _ (seed *ds* run-id)
+        tc (component/start (trace/make {:datasource *ds* :batch-size 10 :flush-ms 20}))
+        ch (:events-ch tc)]
+    (try
+      (dotimes [i 3]
+        (a/>!! ch {:event-id (random-uuid) :run-id run-id :at i :kind :recv}))
+      (Thread/sleep 100)
+      (is (= 3 (count (store/get-events *ds* run-id nil))))
+      (finally (component/stop tc)))))
